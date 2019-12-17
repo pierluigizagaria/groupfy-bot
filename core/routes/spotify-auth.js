eaeb8a33e8935c8888a0792a8e2712caf06a493e
BuildTools <unconfigured@null.spigotmc.org>
2019-12-17 13:12:52 +0100

    Added html pages.

--- core/routes/spotify-auth.js
+++ core/routes/spotify-auth.js
@@ -2,9 +2,10 @@ const express = require('express')
 const accountManager = require('../spotify/account-manager')
 const router = express.Router()
 
-
 router.get('/auth/spotify/callback', accountManager.connectSpotify, (req, res) => {
-    res.successful ? res.send('Spotify successfully connected.') : res.send('Could not connect your spotify account.')
+    res.successful ? 
+    res.sendFile('../public/spotify-auth-success.html') : 
+    res.sendFile('../public/spotify-auth-failed.html')
     res.end()
 })
 
